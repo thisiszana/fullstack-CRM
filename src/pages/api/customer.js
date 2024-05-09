64c1b07e4dc434ff72fd1cf17c27f5b63ee33ce9
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-05-09 16:37:29 +0330

    Create api with POST method to get customer data.

---
+++ src/pages/api/customer.js
@@ -0,0 +1,39 @@
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: "Failed", message: "Error in connection DB!" });
+  }
+
+  if (req.method === "POST") {
+    const data = req.body.data;
+
+    if (!data.name || !data.lastname || !data.email) {
+      return res
+        .status(400)
+        .json({ status: "Bad Request", message: "Invalid Data" });
+    }
+
+    try {
+      const customer = await Customer.create(data);
+      
+      res.status(201).json({
+        status: "Success",
+        message: "Customer created successfully",
+        data: customer,
+      });
+
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ status: "feild", message: "error in storing in DB!" });
+    }
+  }
+}
