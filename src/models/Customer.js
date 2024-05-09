8c43d7f696336cd288290cfb8f2e9f38a8541540
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-05-09 15:43:51 +0330

    Creating the Customer model.

---
+++ src/models/Customer.js
@@ -0,0 +1,36 @@
+import { Schema, model, models } from "mongoose";
+
+const CustomerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  phone: String,
+  address: String,
+  postalCode: Number,
+  date: Date,
+  products: {
+    type: Array,
+    default: [],
+  },
+  createAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+  uppdatedAt: { type: Date, default: () => Date.now() },
+});
+
+const Customer = models.Customer || model("Customer", CustomerSchema);
+export default Customer;
